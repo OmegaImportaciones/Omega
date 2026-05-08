de9d61f0abd2cc07c29a8b726c9143abd4766a4f
OmegaImportaciones <importacionesomega280@gmail.com>
2026-05-08 19:42:24 -0400

    Add files via upload

--- js/catalog/navbar.js
+++ js/catalog/navbar.js
@@ -2,51 +2,19 @@ const header =
     document.getElementById('catalogHeader');
 
 
-/* =========================
-   SCROLL ANIMATION
-========================= */
-
 window.addEventListener('scroll', () => {
 
-    const scrollY =
-        window.scrollY;
+    if (window.scrollY > 80) {
 
-    const maxScroll =
-        180;
-
-
-    /* =========================
-       PROGRESS
-    ========================= */
-
-    const progress =
-        Math.min(
-            scrollY / maxScroll,
-            1
+        header.classList.add(
+            'compact-active'
         );
-
-
-    /* =========================
-       UPDATE CSS VARIABLE
-    ========================= */
-
-    header.style.setProperty(
-        '--compact-progress',
-        progress
-    );
-
-
-    /* =========================
-       OPTIONAL SHADOW STATE
-    ========================= */
-
-    if (scrollY > 10) {
-
-        header.classList.add('compact-active');
 
     } else {
 
-        header.classList.remove('compact-active');
+        header.classList.remove(
+            'compact-active'
+        );
 
     }
 
